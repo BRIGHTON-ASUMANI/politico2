4369f08637b30633736aea9dade7b445feb00670
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-02-06 02:07:49 +0300

    created a create political party route

---
+++ app/api/version1/__init__.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+v1 = Blueprint('version1', __name__, url_prefix='/api/v1')
+
+
+from app.api.version1.views import party_views
---
+++ app/api/version1/views/party_views.py
@@ -0,0 +1,29 @@
+from flask import jsonify, make_response, request
+from app.api.version1 import v1
+from app.api.version1.models.party_models import Party, parties
+
+@v1.route('/parties', methods=['GET'])
+def get_all_parties():
+    res = Party.get_parties()
+    return make_response(jsonify({
+        'message': res
+    }), 200)
+
+@v1.route('/createparty', methods=['POST'])
+def create_parties():
+    party = request.get_json()
+    party_name = ['name']
+    party_hq = ['hqAddress']
+    party_logo = ['logoUrl']
+
+    party = {
+        'name': party_name,
+        'hqAddress': party_hq,
+        'logoUrl': party_logo
+    }
+    parties.append(party)
+    c_party = Party.create_party(party)
+    return make_response(jsonify({
+        'message': 'Party added successfully'
+    }), 200)
+
