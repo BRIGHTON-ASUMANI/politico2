352a793e28c80e56beb24a4549625ad08e3fd6b0
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-02-06 20:05:03 +0300

    [Starts #163714072] created a /createoffice route

--- app/api/version1/views/party_views.py
+++ app/api/version1/views/party_views.py
@@ -20,10 +20,28 @@ def create_parties():
         'message': 'Party added successfully'
     }), 200)
 
-
 @v1.route('/parties', methods=['GET'])
 def get_all_parties():
     res = Party.get_parties()
     return make_response(jsonify({
         'message': res
+    }), 200)
+
+
+@v1.route('/createoffice', methods=['POST'])
+def create_offices():
+    office = request.get_json()
+    office_type = ['type']
+    office_name = ['name']
+    
+
+    new_office = {
+        'type': office_type,
+        'name': office_name
+        
+    }
+    parties.append(party)
+    Party.create_party(new_party)
+    return make_response(jsonify({
+        'message': 'Office added successfully'
     }), 200)
