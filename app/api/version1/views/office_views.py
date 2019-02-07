1f6c5984484927d92284d2102ecd0290589d11ba
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-02-08 00:49:59 +0300

    [Bug #163793033] added docstrings to the whole application and completed the GET and POST route for the offices /offices

--- app/api/version1/views/office_views.py
+++ app/api/version1/views/office_views.py
@@ -1,24 +1,34 @@
 from flask import jsonify, make_response, request
 from app.api.version1 import v1
-from app.api.version1.models.office_models import Office, all_offices
+from app.api.version1.models.office_models import Office, offices
 
-@v1.route('/createoffice', methods=['POST'])
-def create_offices():
+
+@v1.route('/offices', methods=['POST'])
+def create_office():
+    '''
+    method for creating an office
+    '''
     office = request.get_json()
-    office_type = ['type']
-    office_name = ['name']
-    
-    new_office = {
-        "type": office_type,
-        "name": office_name
-    }
-    all_offices.append(office)
-    Office.create_office(new_office)
-    return make_response(jsonify({
-        'message': 'Office added successfully'
-    }), 200)
+    office_name = office['name']
+    office_type = office['type']
+     
+    response = Office().create_office(office_name, office_type)
+    return make_response(jsonify(response = {
+        'message': 'Office added successfully',
+        'status': 201,
+        'data': response
+    }), 201)
 
 @v1.route('/offices', methods=['GET'])
 def get_all_offices():
-    return Office.get_office()
+    '''
+    method for getting all offfices
+    '''
+    all_offices = Office().get_offices()
+    return make_response(jsonify({
+        'message': 'All Offices',
+        'status': 200,
+        'data':all_offices
+    }), 200)
+
 
--- app/api/version1/views/party_views.py
+++ app/api/version1/views/party_views.py
@@ -8,12 +8,15 @@ from app.api.version1.models.party_models import Party, parties
 
 @v1.route('/parties', methods=['POST'])
 def create_party():
+    '''
+    method for creating a party
+    '''
     party = request.get_json()
     name = party['name']
     hqAddress = party['hqAddress']
     logoUrl = party['logoUrl']
 
-    
+
     response = Party().create_party(name, hqAddress, logoUrl)
     return make_response(jsonify(response = {
         'message': 'Party added successfully',
@@ -23,7 +26,9 @@ def create_party():
 
 @v1.route('/parties', methods=['GET'])
 def get_all_parties():
-
+    '''
+    method for getting all parties
+    '''
     all_parties = Party().get_parties()
     return make_response(jsonify({
         'message': 'All parties',
