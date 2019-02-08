2c9e9460c558e5119b6f130289f42c488e6ae788
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-02-08 12:08:25 +0300

    [Delivers #163713113] completed the specific-office route

--- app/api/version1/models/office_models.py
+++ app/api/version1/models/office_models.py
@@ -29,14 +29,14 @@ class Office():
         return self.all_offices
 
     def get_specific_office(self, office_id):
-    '''
-        Args:
-        office_id: specific id of a office.
-        
-    '''
-    if self.all_offices:
-        for spec_office in self.all_offices:
-            if spec_office.get('office_id') == office_id:
+        '''
+            Args:
+            office_id: specific id of a office.
+            
+        '''
+        if self.all_offices:
+            for spec_office in self.all_offices:
+                if spec_office.get('office_id') == office_id:
 
-                return spec_office
+                    return spec_office
 
--- app/api/version1/views/office_views.py
+++ app/api/version1/views/office_views.py
@@ -53,3 +53,23 @@ def get_all_offices():
     }), 200)
 
 
+@v1.route('/offices/<int:office_id>', methods=['GET'])
+def get_specific_office(office_id):
+    '''
+    method for getting a specific office
+    '''
+    specific_office = Office().get_specific_office(office_id)
+    if specific_office:
+        return make_response(jsonify({
+            'message': 'success',
+            'Status': 200,
+            'parties': specific_office
+        }), 200)
+    return make_response(jsonify({
+        'status': 404,
+        'message': 'office not found'
+    }), 404)
+
+
+
+
