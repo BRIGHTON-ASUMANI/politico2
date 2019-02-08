0a2a2a3319f94bf76da6f30fa8d59a6cb7fbadb0
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2019-02-08 11:57:17 +0300

    [Starts #163713113] added a specific office method in the office models

--- app/api/version1/models/office_models.py
+++ app/api/version1/models/office_models.py
@@ -27,5 +27,16 @@ class Office():
 
         self.all_offices.append(new_office)
         return self.all_offices
+
+    def get_specific_office(self, office_id):
+    '''
+        Args:
+        office_id: specific id of a office.
         
+    '''
+    if self.all_offices:
+        for spec_office in self.all_offices:
+            if spec_office.get('office_id') == office_id:
+
+                return spec_office
 
